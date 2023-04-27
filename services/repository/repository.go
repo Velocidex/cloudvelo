c60ebe0e9f0583282c804e3be9eb625ffe17ad5e
Mike Cohen <mike@velocidex.com>
2023-04-27 11:58:59 +1000

    Ensure artifacts are compiled on all paths.

--- services/repository/repository.go
+++ services/repository/repository.go
@@ -184,7 +184,7 @@ func (self *Repository) Get(
 	if err == nil {
 		artifact, ok := artifact_any.(*artifacts_proto.Artifact)
 		if ok {
-			return artifact, true
+			return self.ensureCompiled(config_obj, artifact)
 		}
 	}
 
@@ -193,17 +193,48 @@ func (self *Repository) Get(
 	if self.parent != nil {
 		artifact, pres := self.parent.Get(ctx, self.parent_config_obj, name)
 		if pres {
-			return artifact, true
+			return self.ensureCompiled(config_obj, artifact)
 		}
 	}
 
 	// Failing this we try to read from the backend.
-	artifact, pres := self.getFromBackend(config_obj, name)
-	if !pres {
-		return nil, pres
+	return self.getFromBackend(config_obj, name)
+}
+
+// Make sure the artifact is compiled.
+func (self *Repository) ensureCompiled(
+	config_obj *config_proto.Config,
+	artifact *artifacts_proto.Artifact) (*artifacts_proto.Artifact, bool) {
+
+	if artifact.Compiled {
+		return artifact, true
 	}
 
-	return artifact, true
+	// Compile the artifact from the backend and return it.
+	dummy_repository := repository.Repository{
+		Data: make(map[string]*artifacts_proto.Artifact),
+	}
+
+	artifact, err := dummy_repository.LoadProto(
+		artifact, services.ArtifactOptions{
+			ArtifactIsBuiltIn:    artifact.BuiltIn,
+			ArtifactIsCompiledIn: artifact.CompiledIn,
+			ValidateArtifact:     false,
+		})
+	if err != nil {
+		return nil, false
+	}
+
+	compiled_artifact, pres := dummy_repository.Get(
+		self.ctx, self.config_obj, artifact.Name)
+	if !pres {
+		compiled_artifact = artifact
+	}
+
+	// Remember it for next time.
+	self.lru.Set(compiled_artifact.Name, compiled_artifact)
+
+	return compiled_artifact, true
 }
 
 func (self *Repository) getFromBackend(
@@ -228,35 +259,7 @@ func (self *Repository) getFromBackend(
 		return nil, false
 	}
 
-	if artifact.Compiled {
-		return artifact, true
-	}
-
-	// Compile the artifact from the backend and return it.
-	dummy_repository := repository.Repository{
-		Data: make(map[string]*artifacts_proto.Artifact),
-	}
-
-	artifact, err = dummy_repository.LoadProto(
-		artifact, services.ArtifactOptions{
-			ArtifactIsBuiltIn:    artifact.BuiltIn,
-			ArtifactIsCompiledIn: artifact.CompiledIn,
-			ValidateArtifact:     false,
-		})
-	if err != nil {
-		return nil, false
-	}
-
-	compiled_artifact, pres := dummy_repository.Get(
-		self.ctx, self.config_obj, artifact.Name)
-	if !pres {
-		compiled_artifact = artifact
-	}
-
-	// Remember it for next time.
-	self.lru.Set(compiled_artifact.Name, compiled_artifact)
-
-	return compiled_artifact, true
+	return self.ensureCompiled(config_obj, artifact)
 }
 
 func (self *Repository) saveArtifact(
