9412b2f59dc0cdcf6b1e6bb95227ede8753bb7cb
Eamon Mc Gee <eamon_mcgee@rapid7.com>
2024-03-28 10:12:34 +0000

    test fix

--- vql/server/clients/delete.go
+++ vql/server/clients/delete.go
@@ -62,7 +62,7 @@ func (self DeleteClientPlugin) Call(ctx context.Context,
 			return
 		}
 
-		indexes := []string{"persisted"}
+		indexes := []string{"transient", "persisted"}
 		for _, index := range indexes {
 			if arg.ReallyDoIt {
 				err = removeClientDocs(ctx, config_obj, index, arg.ClientId)
