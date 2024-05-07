9e4aba99844860e0f71fe27fac12580c07e667b1
Eamon Mc Gee <eamon_mcgee@rapid7.com>
2024-05-07 09:18:04 +0100

    updating opensearch query

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -65,7 +65,7 @@ const (
         "bool": {
             "must": [
                 {
-                    "match": {
+                    "prefix": {
                         "id": %q
                     }
                 },
