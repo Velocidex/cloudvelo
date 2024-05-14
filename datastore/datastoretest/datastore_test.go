4f8ef611e5c059beeb8caf535d187fed464b13a6
Eamon Mc Gee <eamon_mcgee@rapid7.com>
2024-05-14 12:34:04 +0100

    test for queries

--- datastore/datastoretest/datastore_test.go
+++ datastore/datastoretest/datastore_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
+	"os"
 	"testing"
 	"www.velocidex.com/golang/cloudvelo/datastore"
 	cvelo_services "www.velocidex.com/golang/cloudvelo/services"
@@ -40,21 +41,21 @@ type DatastoreTest struct {
 	ctx context.Context
 }
 
-func (self *DatastoreTest) TestDownloadQuery() {
-
+func (self *DatastoreTest) TestDownloadQueriesOnTransientIndex() {
+	var vfs_path = "/downloads/hunts/H.CP12M5IRRKRUE/H.CP12M5IRRKRUE.json.db"
 	var serialized = "\"{\"timestamp\":1715614088,\"components\":[\"downloads\",\"hunts\",\"H.CP12M5IRRKRUE\",\"H.CP12M5IRRKRUE.zip\"],\"type\":\"zip\"}\""
 	record := datastore.DatastoreRecord{
-		ID:        cvelo_services.MakeId("/downloads/hunts/H.CP12M5IRRKRUE/H.CP12M5IRRKRUE.json.db"),
+		ID:        cvelo_services.MakeId(vfs_path),
 		Type:      "Generic",
-		VFSPath:   "/downloads/hunts/H.CP12M5IRRKRUE/H.CP12M5IRRKRUE.json.db",
+		VFSPath:   vfs_path,
 		JSONData:  serialized,
 		DocType:   "datastore",
 		Timestamp: utils.GetTime().Now().UnixNano(),
 	}
 
-	cvelo_services.SetElasticIndex(self.ctx,
+	err := cvelo_services.SetElasticIndex(self.ctx,
 		"test", "transient", "", record)
-	var vfs_path = "/downloads/hunts/H.CP12M5IRRKRUE/H.CP12M5IRRKRUE.json.db"
+
 	serialized = "{\"timestamp\":1715614088,\"total_uncompressed_bytes\":21375,\"total_compressed_bytes\":20054,\"total_container_files\":18,\"hash\":\"095079e35e17c37bd5ac6f602a15173697f404a2ac9ea4b1f54c653ab25706e2\",\"total_duration\":1,\"components\":[\"downloads\",\"hunts\",\"H.CP12M5IRRKRUE\",\"H.CP12M5IRRKRUE.zip\"],\"type\":\"zip\"}"
 	record = datastore.DatastoreRecord{
 		ID:        cvelo_services.MakeId(vfs_path),
@@ -64,20 +65,22 @@ func (self *DatastoreTest) TestDownloadQuery() {
 		DocType:   "datastore",
 		Timestamp: utils.GetTime().Now().UnixNano(),
 	}
-	cvelo_services.SetElasticIndex(self.ctx,
+	err = cvelo_services.SetElasticIndex(self.ctx,
 		"test", "transient", "", record)
 
 	id := cvelo_services.MakeId(vfs_path)
-	_, err := cvelo_services.GetElasticRecord(
-		self.ctx, self.OrgId, "transient", id)
+	hit, err := cvelo_services.GetElasticRecord(
+		self.ctx, "test", "transient", id)
+	assert.Nil(self.T(), hit)
+	if assert.Error(self.T(), err) {
+		assert.Equal(self.T(), os.ErrNotExist, err)
+	}
+	result, _, err := cvelo_services.QueryElasticRaw(self.ctx, "test",
+		"transient", json.Format(get_datastore_doc_query, cvelo_services.MakeId(vfs_path)))
 
 	assert.NoError(self.T(), err)
-
-	_, _, err = cvelo_services.QueryElasticRaw(self.ctx, self.OrgId,
-		"transient", json.Format(get_datastore_doc_query, cvelo_services.MakeId("/downloads/hunts/H.CP12M5IRRKRUE/H.CP12M5IRRKRUE.json.db")))
-	assert.NoError(self.T(), err)
+	assert.Equal(self.T(), 1, len(result))
 }
-
 func TestDataStore(t *testing.T) {
 	suite.Run(t, &DatastoreTest{
 		CloudTestSuite: &testsuite.CloudTestSuite{
